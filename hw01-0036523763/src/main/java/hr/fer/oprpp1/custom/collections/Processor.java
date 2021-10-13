cb04b6184c2ce19e02a19663ed4bed506793a2c2
Lovro Grgurić Mileusnić <lovro.grguric-mileusnic@fer.hr>
2021-10-13 18:45:26 +0200

    Started adding tests for ArrayIndexedCollection

--- hw01-0036523763/src/main/java/hr/fer/oprpp1/custom/collections/Processor.java
+++ hw01-0036523763/src/main/java/hr/fer/oprpp1/custom/collections/Processor.java
@@ -1,5 +1,13 @@
 package hr.fer.oprpp1.custom.collections;
 
+/**
+ * Model of an object which performs one action on passed object.
+ * The action must be defined in the process method, when implementing subclasses of the processor.
+ */
 public class Processor {
+    /**
+     * Action to be performed on the passed object.
+     * @param value reference of the object on which the action will be performed
+     */
     public void process(Object value) {};
 }
--- hw01-0036523763/src/test/java/hr/fer/oprpp1/custom/collections/ArrayIndexedCollectionTest.java
+++ hw01-0036523763/src/test/java/hr/fer/oprpp1/custom/collections/ArrayIndexedCollectionTest.java
@@ -1,2 +1,95 @@
-package hr.fer.oprpp1.custom.collections;public class ArrayIndexedCollectionTest {
+package hr.fer.oprpp1.custom.collections;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class ArrayIndexedCollectionTest {
+
+    @Test
+    public void testIllegalInitialCapacity() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            ArrayIndexedCollection a = new ArrayIndexedCollection(-1);
+        });
+    }
+
+    @Test
+    public void testPassedCollectionIsNull() {
+        assertThrows(NullPointerException.class, () -> {
+            ArrayIndexedCollection a = new ArrayIndexedCollection(null);
+        });
+    }
+
+    @Test
+    public void testPassedCollectionIsNullInTwoArgumentConstructor() {
+        assertThrows(NullPointerException.class, () -> {
+            ArrayIndexedCollection a = new ArrayIndexedCollection(null, 16);
+        });
+    }
+
+    @Test
+    public void testIllegalInitialCapacityInTwoArgumentConstructor() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            ArrayIndexedCollection a = new ArrayIndexedCollection(new ArrayIndexedCollection(), -1);
+        });
+    }
+
+    @Test
+    public void testAddingNullValue() {
+        ArrayIndexedCollection a = new ArrayIndexedCollection();
+        assertThrows(NullPointerException.class, () -> {
+            a.add(null);
+        });
+    }
+
+    @Test
+    public void testSizeWhenAddingValues() {
+        ArrayIndexedCollection a = new ArrayIndexedCollection();
+        a.add(1);
+        a.add(2);
+        assertEquals(2,a.size() );
+    }
+
+    @Test
+    public void testElementsWhenAddingValues() {
+        ArrayIndexedCollection a = new ArrayIndexedCollection();
+        a.add(1);
+        a.add(2);
+        assertEquals(new Object[] {1,2}, a.toArray());
+    }
+
+    @Test
+    public void testAddingElementWhenCollectionIsFull() {
+        ArrayIndexedCollection a = new ArrayIndexedCollection(2);
+        a.add(1);
+        a.add(2);
+        a.add(3);
+        assertEquals(new Object[] {1,2,3}, a.toArray());
+    }
+
+    @Test
+    public void testContainsValue() {
+        ArrayIndexedCollection a = new ArrayIndexedCollection(2);
+        a.add(1);
+        a.add(2);
+        assertTrue(a.contains(2));
+    }
+
+    @Test
+    public void testDoesntContainValue() {
+        ArrayIndexedCollection a = new ArrayIndexedCollection(2);
+        a.add(1);
+        a.add(2);
+        assertFalse(a.contains(3));
+    }
+
+    @Test
+    public void IllegalIndexRemoveMethod() {
+        ArrayIndexedCollection a = new ArrayIndexedCollection(2);
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            a.remove(-1);
+        });
+    }
 }
