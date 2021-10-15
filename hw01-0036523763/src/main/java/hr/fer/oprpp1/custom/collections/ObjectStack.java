24b6b1771ad007204dc3a1f7064b2ea070ccde4d
Lovro Grgurić Mileusnić <lovro.grguric-mileusnic@fer.hr>
2021-10-15 17:33:01 +0200

    Added ObjectStack.java

---
+++ hw01-0036523763/src/main/java/hr/fer/oprpp1/custom/collections/ObjectStack.java
@@ -0,0 +1,40 @@
+package hr.fer.oprpp1.custom.collections;
+
+/**
+ * {@code ObjectStack} is an adaptor class adapting {@code ArrayIndexedCollection} to provide its users with methods natural for a stack.
+ * {@code ArrayIndexedCollection} is used for storage of the "stack's" elements.
+ */
+
+public class ObjectStack {
+    private ArrayIndexedCollection storage;
+
+    public ObjectStack() {
+        storage = new ArrayIndexedCollection();
+    }
+
+    public boolean isEmpty() {
+        return storage.isEmpty();
+    }
+
+    public int size() {
+        return storage.size();
+    }
+
+    public void push(Object value) {
+        storage.add(value);
+    }
+
+    public Object pop() {
+        Object element = storage.get(storage.size() - 1);
+        storage.remove(storage.size() - 1);
+        return element;
+    }
+
+    public Object peek() {
+        return storage.get(storage.size() - 1);
+    }
+
+    public void clear() {
+        storage.clear();
+    }
+}
