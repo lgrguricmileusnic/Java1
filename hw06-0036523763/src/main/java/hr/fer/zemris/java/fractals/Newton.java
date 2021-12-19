fed42ab522d25a5ccdb7303f5c2e970bf8bb576f
Lovro Grgurić Mileusnić <lovro.grguric-mileusnic@fer.hr>
2021-12-19 19:29:00 +0100

    Paralelno dovrseno

--- hw06-0036523763/src/main/java/hr/fer/zemris/java/fractals/Newton.java
+++ hw06-0036523763/src/main/java/hr/fer/zemris/java/fractals/Newton.java
@@ -12,6 +12,8 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import static hr.fer.zemris.math.utils.ComplexParser.parseComplex;
+
 public class Newton {
     private static final String WELCOME_MESSAGE = "Welcome to Newton-Raphson iteration-based fractal viewer.\n" +
             "Please enter at least two roots, one root per line. Enter 'done' when one.";
@@ -43,16 +45,16 @@ public class Newton {
     }
 
     public static class NewtonProducer implements IFractalProducer {
-        private final ComplexRootedPolynomial rootedPolynom;
+        private final ComplexRootedPolynomial rootedPolynomial;
         private final ComplexPolynomial derived;
-        private final ComplexPolynomial polynom;
+        private final ComplexPolynomial polynomial;
         private final double convergenceThreshold;
         private final double rootThreshold;
 
         public NewtonProducer(ComplexRootedPolynomial rootedPolynom, double convergenceTreshold, double rootTreshold) {
-            this.rootedPolynom = rootedPolynom;
-            this.polynom = rootedPolynom.toComplexPolynom();
-            this.derived = polynom.derive();
+            this.rootedPolynomial = rootedPolynom;
+            this.polynomial = rootedPolynom.toComplexPolynom();
+            this.derived = polynomial.derive();
             this.convergenceThreshold = convergenceTreshold;
             this.rootThreshold = rootTreshold;
         }
@@ -70,105 +72,22 @@ public class Newton {
                     double cre = x / (width-1.0) * (reMax - reMin) + reMin;
                     double cim = (height-1.0-y) / (height-1) * (imMax - imMin) + imMin;
                     Complex zn = new Complex(cre, cim);
-                    Complex znOld;
                     double module;
                     int iter = 0;
                     do {
-                        Complex numerator = rootedPolynom.apply(zn);
+                        Complex numerator = rootedPolynomial.apply(zn);
                         Complex denominator = derived.apply(zn);
+                        Complex znold = zn;
                         Complex fraction = numerator.divide(denominator);
-                        Complex zn1 = zn.sub(fraction);
-                        module = zn1.sub(zn).module();
-                        zn = zn1;
+                        zn = zn.sub(fraction);
+                        module = znold.sub(zn).module();
                         iter++;
                     }while(module > convergenceThreshold && iter < m);
-                    int index = rootedPolynom.indexOfClosestRootFor(zn, rootThreshold);
+                    int index = rootedPolynomial.indexOfClosestRootFor(zn, rootThreshold);
                     data[offset++] = (short)(index + 1);
                 }
             }
-            observer.acceptResult(data, (short)(polynom.order()+1), requestNo);
+            observer.acceptResult(data, (short)(polynomial.order()+1), requestNo);
         }
-    }
-
-    public static Complex parseComplex(String text) {
-        if (text.isBlank() || text.isEmpty()) throw new NumberFormatException("Invalid input");
-        int i = 0;
-        String a, b;
-        boolean nextNegative = false;
-        char c;
-
-        text = text.replaceAll("\\s", "");
-        if (text.startsWith("-")) {
-            nextNegative = true;
-            text = text.replaceFirst("-", "");
-        }
-
-        a = "";
-        c = text.charAt(0);
-        if (Character.isDigit(c)) {
-            a = "";
-
-            while (i < text.length()) {
-                c = text.charAt(i);
-                if (!Character.isDigit(c) && c != '.') break;
-                a += c;
-                i++;
-            }
-            if (nextNegative) {
-                a = "-" + a;
-                nextNegative = false;
-            }
-            if (i >= text.length()) { //if while loop ended naturally
-                try {
-                    return new Complex(Double.parseDouble(a), 0.0);
-                } catch (NumberFormatException e) {
-                    throw new NumberFormatException("Invalid format");
-                }
-
-            }
-            if (c == 'i' && i >= text.length() - 1) {
-                try {
-                    return new Complex(0.0, Double.parseDouble(a));
-                } catch (NumberFormatException e) {
-                    throw new NumberFormatException("Invalid format");
-                }
-            }
-            if (c != '+' && c != '-') throw new NumberFormatException("Invalid input");
-            if (c == '-') {
-                nextNegative = true;
-            }
-            i++;
-        }
-        try {
-            c = text.charAt(i);
-        } catch (IndexOutOfBoundsException e) {
-            throw new NumberFormatException("Invalid input");
-        }
-        if (c != 'i') {
-            throw new NumberFormatException("Invalid input");
-        }
-        b = "";
-        i++;
-        while (i < text.length()) {
-            c = text.charAt(i);
-            if (!Character.isDigit(c) && c != '.') break;
-            b += c;
-            i++;
-        }
-        if (b.isEmpty()) {
-            b = "1";
-        }
-        if (a.isEmpty()) {
-            a = "0";
-        }
-        if (nextNegative) {
-            b = "-" + b;
-        }
-        try {
-            return new Complex(Double.parseDouble(a), Double.parseDouble(b));
-        } catch (NumberFormatException e) {
-            throw new NumberFormatException("Invalid format");
-        }
-
     }
 }
--- hw06-0036523763/src/main/java/hr/fer/zemris/java/fractals/NewtonParallel.java
+++ hw06-0036523763/src/main/java/hr/fer/zemris/java/fractals/NewtonParallel.java
@@ -1,2 +1,269 @@
-package hr.fer.zemris.java.fractals;public class NewtonParallel {
+package hr.fer.zemris.java.fractals;
+
+import hr.fer.zemris.java.fractals.viewer.FractalViewer;
+import hr.fer.zemris.java.fractals.viewer.IFractalProducer;
+import hr.fer.zemris.java.fractals.viewer.IFractalResultObserver;
+import hr.fer.zemris.math.Complex;
+import hr.fer.zemris.math.ComplexPolynomial;
+import hr.fer.zemris.math.ComplexRootedPolynomial;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static hr.fer.zemris.math.utils.ComplexParser.parseComplex;
+
+public class NewtonParallel {
+    private static final String WELCOME_MESSAGE = "Welcome to Newton-Raphson iteration-based fractal viewer.\n" +
+            "Please enter at least two roots, one root per line. Enter 'done' when one.";
+
+    static double convergenceThreshold;
+    static double rootThreshold;
+    static ComplexRootedPolynomial rootedPolynomial;
+    static ComplexPolynomial derived;
+    static ComplexPolynomial polynomial;
+    static int workers;
+    static int k;
+
+
+    public static void main(String[] args) {
+        int i = 0;
+        workers = 0;
+        k = 0;
+        while(i < args.length) {
+            String option = args[i];
+            if(option.startsWith("-w")){
+                if (workers != 0){
+                    System.out.println("Workers option specified more than once.");
+                    System.exit(1);
+                }
+                try {
+                    workers = Integer.parseInt(args[++i]);
+                } catch (NumberFormatException | IndexOutOfBoundsException e) {
+                    System.out.println("Invalid options");
+                    System.exit(1);
+                }
+
+            } else if (option.startsWith("--workers=")) {
+                if (workers != 0){
+                    System.out.println("Workers option specified more than once.");
+                    System.exit(1);
+                }
+                try {
+                    workers = Integer.parseInt(option.substring(option.indexOf('=') + 1));
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid options");
+                    System.exit(1);
+                }
+            } else if (option.startsWith("-t")) {
+                if (k != 0){
+                    System.out.println("Tracks option specified more than once.");
+                    System.exit(1);
+                }
+                try {
+                    k = Integer.parseInt(args[++i]);
+                } catch (NumberFormatException | IndexOutOfBoundsException e) {
+                    System.out.println("Invalid options");
+                    System.exit(1);
+                }
+                if (k < 1) {
+                    System.out.println("Number of tracks must be greater than zero.");
+                    System.exit(1);
+                }
+            } else if (option.startsWith("--tracks=")) {
+                if (k != 0){
+                    System.out.println("Tracks option specified more than once.");
+                    System.exit(1);
+                }
+                try {
+                    k = Integer.parseInt(option.substring(option.indexOf('=') + 1));
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid options");
+                    System.exit(1);
+                }
+            } else {
+                System.out.println("Unknown option: " + option);
+                System.exit(1);
+            }
+            i++;
+        }
+        if(workers == 0) workers = Runtime.getRuntime().availableProcessors();
+        System.out.println(WELCOME_MESSAGE);
+        Scanner sc = new Scanner(System.in);
+        int rootNo = 0;
+        List<Complex> roots = new ArrayList<>();
+        while (true) {
+            System.out.print("Root " + rootNo + "> ");
+            String line = sc.nextLine();
+            if (line.strip().equalsIgnoreCase("done")) break;
+            try{
+                roots.add(parseComplex(line));
+                System.out.println(parseComplex(line));
+                rootNo++;
+            }catch(NumberFormatException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        if(roots.size() < 2) {
+            System.out.println("Not enough roots entered.");
+            System.exit(1);
+        }
+        convergenceThreshold = 0.001;
+        rootThreshold = 0.002;
+        rootedPolynomial = new ComplexRootedPolynomial(Complex.ONE, roots.toArray(roots.toArray(new Complex[0])));
+        polynomial = rootedPolynomial.toComplexPolynom();
+        derived = polynomial.derive();
+        FractalViewer.show(new NewtonProducer());
+    }
+
+    public static class PosaoIzracuna implements Runnable {
+        double reMin;
+        double reMax;
+        double imMin;
+        double imMax;
+        int width;
+        int height;
+        int yMin;
+        int yMax;
+        int m;
+        short[] data;
+        AtomicBoolean cancel;
+
+
+        public static PosaoIzracuna NO_JOB = new PosaoIzracuna();
+
+        private PosaoIzracuna() {
+        }
+
+        public PosaoIzracuna(double reMin, double reMax, double imMin,
+                             double imMax, int width, int height, int yMin, int yMax,
+                             int m, short[] data, AtomicBoolean cancel) {
+            super();
+            this.reMin = reMin;
+            this.reMax = reMax;
+            this.imMin = imMin;
+            this.imMax = imMax;
+            this.width = width;
+            this.height = height;
+            this.yMin = yMin;
+            this.yMax = yMax;
+            this.m = m;
+            this.data = data;
+            this.cancel = cancel;
+        }
+
+        @Override
+        public void run() {
+
+            int offset = yMin * width;
+
+            for(int y = yMin; y <= yMax && !cancel.get(); ++y) {
+                for(int x = 0; x < width; ++x) {
+                    double cre = x / (width-1.0) * (reMax - reMin) + reMin;
+                    double cim = (height-1.0-y) / (height-1) * (imMax - imMin) + imMin;
+                    Complex zn = new Complex(cre, cim);
+                    double module;
+                    int iter = 0;
+                    do {
+                        Complex numerator = rootedPolynomial.apply(zn);
+                        Complex denominator = derived.apply(zn);
+                        Complex znold = zn;
+                        Complex fraction = numerator.divide(denominator);
+                        zn = zn.sub(fraction);
+                        module = znold.sub(zn).module();
+                        iter++;
+                    } while(module > convergenceThreshold && iter < m);
+                    int index = rootedPolynomial.indexOfClosestRootFor(zn, rootThreshold);
+                    data[offset++] = (short)(index + 1);
+                }
+            }
+
+        }
+    }
+
+    public static class NewtonProducer implements IFractalProducer {
+
+
+        public NewtonProducer() {
+        }
+
+        @Override
+        public void produce(double reMin, double reMax, double imMin, double imMax,
+                            int width, int height, long requestNo, IFractalResultObserver observer, AtomicBoolean cancel) {
+            System.out.println("Zapocinjem izracun...");
+            int m = 16 * 16 * 16;
+            short[] data = new short[width * height];
+            final int brojTraka;
+            if (k != 0) {
+                 brojTraka = Math.min(k, height);
+            } else {
+                brojTraka = height;
+            }
+
+            int brojYPoTraci = height / brojTraka;
+
+            final BlockingQueue<PosaoIzracuna> queue = new LinkedBlockingQueue<>();
+
+            Thread[] radnici = new Thread[workers];
+            for (int i = 0; i < radnici.length; i++) {
+                radnici[i] = new Thread(() -> {
+                    while (true) {
+                        PosaoIzracuna p = null;
+                        try {
+                            p = queue.take();
+                            if (p == PosaoIzracuna.NO_JOB) break;
+                        } catch (InterruptedException e) {
+                            continue;
+                        }
+                        p.run();
+                    }
+                });
+            }
+            for (int i = 0; i < radnici.length; i++) {
+                radnici[i].start();
+            }
+
+            for (int i = 0; i < brojTraka; i++) {
+                int yMin = i * brojYPoTraci;
+                int yMax = (i + 1) * brojYPoTraci - 1;
+                if (i == brojTraka - 1) {
+                    yMax = height - 1;
+                }
+                PosaoIzracuna posao = new PosaoIzracuna(reMin, reMax, imMin, imMax, width, height, yMin, yMax, m, data, cancel);
+                while (true) {
+                    try {
+                        queue.put(posao);
+                        break;
+                    } catch (InterruptedException e) {
+                    }
+                }
+            }
+            for (int i = 0; i < radnici.length; i++) {
+                while (true) {
+                    try {
+                        queue.put(PosaoIzracuna.NO_JOB);
+                        break;
+                    } catch (InterruptedException e) {
+                    }
+                }
+            }
+
+            for (int i = 0; i < radnici.length; i++) {
+                while (true) {
+                    try {
+                        radnici[i].join();
+                        break;
+                    } catch (InterruptedException e) {
+                    }
+                }
+            }
+
+            System.out.println("Racunanje gotovo. Idem obavijestiti promatraca tj. GUI!");
+            observer.acceptResult(data, (short) (polynomial.order() + 1), requestNo);
+
+        }
+    }
 }
